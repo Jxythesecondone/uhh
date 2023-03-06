6bbabfdd4b0e9ea26e4700e56c44cf752d8a22ba
suki <105503057+Jxythesecondone@users.noreply.github.com>
2023-03-06 20:12:01 +0530

    Add files via upload

---
+++ java/S.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.applet.*;
+
+public class S extends Applet {
+
+	String si;
+	public void init() {
+		int x=Integer.parseInt(getParameter("P"));
+		int y=Integer.parseInt(getParameter("N"));
+		int z=Integer.parseInt(getParameter("R"));
+		float l=(x*y*z/100);
+		si=String.valueOf(l);
+	}
+}
